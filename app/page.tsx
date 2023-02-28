6a76c920400bfc6a645776755f67428134f8453b
Adobo.io <nox@adobo.io>
2023-02-28 20:13:43 +0800

    Working main page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-//import {useState} from "react";
+'use client'
+
+import {useState} from "react";
+import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
 
 const navigation = [
     { name: 'Product', href: '#' },
@@ -9,11 +13,11 @@ const navigation = [
 ]
 export default function Page() {
 
-//    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
         <div className="isolate bg-white">
-            <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu blur-3xl sm:top-[-20rem]">
+            <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
                 <svg
                     className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
                     viewBox="0 0 1155 678"
@@ -46,38 +50,80 @@ export default function Page() {
                             <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
                         </a>
                     </div>
-                    <div className="flex">
+                    <div className="flex lg:hidden">
                         <button
                             type="button"
                             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(true)}
                         >
                             <span className="sr-only">Open main menu</span>
-                            <Bars3Icon className="h-6 w-6" aria-hidden="false" />
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                         </button>
                     </div>
-                    <div className="lg:flex lg:gap-x-12">
+                    <div className="hidden lg:flex lg:gap-x-12">
                         {navigation.map((item) => (
                             <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
                                 {item.name}
                             </a>
                         ))}
                     </div>
-                    <div className="lg:flex lg:flex-1 lg:justify-end">
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                         <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                            Log in <span aria-hidden="false">&rarr;</span>
+                            Log in <span aria-hidden="true">&rarr;</span>
                         </a>
                     </div>
                 </nav>
+                <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                    <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
+                        <div className="flex items-center justify-between">
+                            <a href="#" className="-m-1.5 p-1.5">
+                                <span className="sr-only">Your Company</span>
+                                <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                            </a>
+                            <button
+                                type="button"
+                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                                onClick={() => setMobileMenuOpen(false)}
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                        </div>
+                        <div className="mt-6 flow-root">
+                            <div className="-my-6 divide-y divide-gray-500/10">
+                                <div className="space-y-2 py-6">
+                                    {navigation.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                                        >
+                                            {item.name}
+                                        </a>
+                                    ))}
+                                </div>
+                                <div className="py-6">
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                                    >
+                                        Log in
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </Dialog.Panel>
+                </Dialog>
             </div>
             <main>
                 <div className="relative px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                        <div className="sm:mb-8 sm:flex sm:justify-center">
+                        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                             <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                                 Announcing our next round of funding.{' '}
                                 <a href="#" className="font-semibold text-indigo-600">
-                                    <span className="absolute inset-0" aria-hidden="false" />
-                                    Read more <span aria-hidden="false">&rarr;</span>
+                                    <span className="absolute inset-0" aria-hidden="true" />
+                                    Read more <span aria-hidden="true">&rarr;</span>
                                 </a>
                             </div>
                         </div>
@@ -97,12 +143,12 @@ export default function Page() {
                                     Get started
                                 </a>
                                 <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                                    Learn more <span aria-hidden="false">→</span>
+                                    Learn more <span aria-hidden="true">→</span>
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu blur-3xl sm:top-[calc(100%-30rem)]">
+                    <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                         <svg
                             className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
                             viewBox="0 0 1155 678"
